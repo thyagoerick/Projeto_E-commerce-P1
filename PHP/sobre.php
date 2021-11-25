6bcd82aaacf4594f1d70b5dbc751637fd7183ed0
Thyago Erick <thyago123gois@gmail.com>
2021-11-25 20:10:20 -0300

    Criando a página sobre.php

--- PHP/sobre.php
+++ PHP/sobre.php
@@ -4,11 +4,64 @@
 <html lang="pt-br">
 <head>
     <title>ELANEON • Sobre Nós</title>
+    <link rel="stylesheet" href="../CSS/sobre.css"/>
 </head>
 <body>
-    <h1 style="text-align: center;">COLOCAR CONTEÚDO AQUI</h1>
+  <section id="sobre">
+    <div id="linkCastelo">
+      <img id="imgCastelo" src="../IMGs/page_images/castelo.png">
+    </div>
+    <div id="historia"> 
+     
+      <h1 class="myFont" style="color:#16C60C;">Sobre Nós &#128154;</h1>
+      <p class="txtHistoria">
+      A nossa história e trajetória na moda já conta com mais de 5 anos. 
+      Acreditamos que as roupas e objetos mais sustentáveis são aqueles que já existem, por isso promovemos a compra e venda de roupas, objetos e acessórios usados em ótimo estado.
+      Com uma curadoria inteligente e responsável, nossa seleção é especializada em peças <strong>SECOND HAND</strong> e também em itens novos, com  qualidade e de marcas nacionais e importadas.
+</p>
+    </div>
+</section>
+
+<section class="duvidas">
+<h1 class="myFont colorTxtG">DÚVIDAS FREQUENTES:</h1>
+    <div id="accordion">
+      <h3>Como saber as medidas do produto?</h3>
+      <div>
+        <p>
+        Colocamos a numeração <b>brasileira</b> na descrição, mas trabalhamos com marcas e grifes do mundo todo e o tamanho de itens de moda costuma variar muito dependendo da modelagem/modelo.
+        Se possível dê preferência para marcas que você já saiba o <b>tamanho</b> que usa.
+        </p>
+      </div>
+      <h3>Esses produtos são usados? São doados?</h3>
+      <div>
+        <p>
+        Sim! Trabalhamos com produtos de <b>segunda-mão</b>, mas sempre em perfeito estado. Não são doações, compramos de pessoas físicas e <b>revendemos</b> na loja física e no site.
+        </p>
+      </div>
+      <h3>Como saber se tem algum defeito? Se a marca de grife é original?</h3>
+      <div>
+        <p>
+        Cada peça passa por uma rigorosa <b>curadoria e análise</b> de qualidade especializada. É muito raro que haja algum problema desse tipo, mas caso receba o produto e note algo errado, você pode realizar uma das ações abaixo: 
+        </p>
+        <ul>
+          <li>Devolução e reembolso;</li>
+          <li>Trocar por um outro produto <i>(na mesma faixa de preço);</i></li>
+          <li>Trocar por um outro produto com o preço um pouco acima e pagar o acréscimo;</li>   
+        </ul>
+      </div>
+      <h3>Coloquei o produto no carrinho e agora está indisponível, o que aconteceu?</h3>
+      <div>
+        <p>
+          Trabalhamos com, grande parte, <b>peças únicas</b>, ou seja, não temos estoque da maioria dessas peças. Se isso aconteceu, significa que outra pessoa colocou na cesta e pagou <b>antes</b> de você.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <script>
+    $( "#accordion" ).accordion();
+  </script>  
 </body>
 </html>
-
 
 <?php include '../PHP/includes/footer.php'; ?>
